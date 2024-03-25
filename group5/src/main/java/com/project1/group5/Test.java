fc5ee4e61a614deae4b5f19e38a2bb88c87d4d42
Marckel9804 <49019179+Marckel9804@users.noreply.github.com>
2024-03-25 11:34:42 +0900

    add project folder
    boot file upload #1

---
+++ group5/src/main/java/com/project1/group5/Test.java
@@ -0,0 +1,13 @@
+package com.project1.group5;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("hello world!");
+    }
+}
