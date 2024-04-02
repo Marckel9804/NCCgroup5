0c11b4e6c1a75905e9ef443fc4854031b75f9048
chjw9806 <chjw9806@naver.com>
2024-04-02 20:13:42 +0900

    내가 커밋함

--- group5/src/main/java/com/project1/group5/db/Jae.java
+++ group5/src/main/java/com/project1/group5/db/Jae.java
@@ -3,5 +3,6 @@ package com.project1.group5.db;
 public class Jae {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("수정!");
     }
 }
