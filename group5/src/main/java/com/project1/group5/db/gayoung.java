a57dd87d70aeb4a62326a8cf301111fc6ce09e83
Gayoung Yoo <olivia378@naver.com>
2024-04-02 20:38:00 +0900

    tnwjd

--- group5/src/main/java/com/project1/group5/db/gayoung.java
+++ group5/src/main/java/com/project1/group5/db/gayoung.java
@@ -4,6 +4,7 @@ public class gayoung {
     public static void main(String[] args) {
         System.out.println("hellow gayoung!");
         System.out.println("안녕");
+        System.out.println("집 가구싶어");
 
     }
 }
