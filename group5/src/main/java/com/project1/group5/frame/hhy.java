3e3ec3006150bba98af41c181d2689e373081d49
Marckel9804 <ckdgh10421@gmail.com>
2024-04-03 19:54:19 +0900

    hhy fix by ch

--- group5/src/main/java/com/project1/group5/frame/hhy.java
+++ group5/src/main/java/com/project1/group5/frame/hhy.java
@@ -5,5 +5,11 @@ public class hhy {
         System.out.println("im changho");
         System.out.println("ㅋㅋㅋ");
         System.out.println("I'm hwayeonh :D");
+
+
+        for(int i =0;i<10;i++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
