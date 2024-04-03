31bdfb7b366c5ba5e4aae9fe29d0744dd51e9d38
Marckel9804 <ckdgh10421@gmail.com>
2024-04-03 19:55:19 +0900

    changho fix by ch

--- group5/src/main/java/com/project1/group5/frame/changho.java
+++ group5/src/main/java/com/project1/group5/frame/changho.java
@@ -4,5 +4,10 @@ public class changho {
     public static void main(String[] args) {
         System.out.println("안녕하세요 저는 안성민입니다.");
         System.out.println("im changho");
+
+
+        for (int i=0;i<5;i++){
+            System.out.println("java jajava");
+        }
     }
 }
